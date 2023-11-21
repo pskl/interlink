a0360c1198c481e7b2346f6e7739a3719739cddc
pskl <hello@pascal.cc>
2023-11-21 13:31:09 +0100

    Add first example of a test run w/ images, adjust UI accordingly

--- docs/script.js
+++ docs/script.js
@@ -1,10 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-  fetch('answers/interlink_mistral_big5/test_7421833607082479695.json')
-  .then(response => response.json())
-  .then(data => {
+const folderPath = "answers/interlink_mistral_big5"
+
+document.addEventListener('DOMContentLoaded', function () {
+  fetch(`${folderPath}/test_9832989361105039657.json`)
+    .then(response => response.json())
+    .then(data => {
       displayBanner(data);
       displayChat(data.answers);
-  });
+      preloadImages(data.answers);
+    });
 });
 
 function displayBanner(data) {
@@ -13,22 +16,48 @@ function displayBanner(data) {
   document.getElementById('prompt').textContent = `${data.prompt}`;
 }
 
+function preloadImages(answers) {
+  const imageDisplay = document.getElementById('image-display');
+  answers.forEach(item => {
+    const img = document.createElement('img');
+    img.src = `${folderPath}/images/question_${item.index}.png`;
+    img.style.display = 'none';
+    imageDisplay.appendChild(img);
+  });
+}
+
 function displayChat(answers) {
   const container = document.getElementById('chat-container');
+  const images = document.getElementById('image-display').children;
+  let delay = 0;
+
   answers.forEach((item, index) => {
-      setTimeout(() => {
-          const questionDiv = document.createElement('div');
-          questionDiv.classList.add('question');
-          questionDiv.textContent = item.question;
-          container.appendChild(questionDiv);
+    // Display question
+    setTimeout(() => {
+      const questionDiv = document.createElement('div');
+      questionDiv.classList.add('question');
+      questionDiv.textContent = item.question;
+      container.appendChild(questionDiv);
 
-          const answerDiv = document.createElement('div');
-          answerDiv.classList.add('answer');
-          answerDiv.textContent = item.sample;
-          container.appendChild(answerDiv);
+      container.scrollTop = container.scrollHeight;
+    }, delay);
 
-          // Scroll to the latest message
-          container.scrollTop = container.scrollHeight;
-      }, index * 1000);
+    delay += 1000; // delay for question to answer transition
+
+    setTimeout(() => {
+      const answerDiv = document.createElement('div');
+      answerDiv.classList.add('answer');
+      answerDiv.textContent = item.sample;
+      container.appendChild(answerDiv);
+
+      container.scrollTop = container.scrollHeight;
+      const img = images[index];
+      img.style.display = 'inline-block';
+      img.scrollIntoView({ behavior: 'smooth', inline: 'start' });
+
+    }, delay);
+
+    delay += 1000; // delay for answer to next question transition
   });
+
 }
